17ac4f8baac6493bc76aed6a48a14539142e4cb9
1_Magistr_Code <ilyha.borec@gmail.com>
2023-05-11 20:30:39 +0600

    commit

--- app/src/main/java/com/example/physics/MainActivity.kt
+++ app/src/main/java/com/example/physics/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.physics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        var bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.formulas -> {
+                    loadFragment(FormulasFragment())
+                    true
+                }
+                R.id.reference_material -> {
+                    loadFragment(DirectoryFragment())
+                    true
+                }
+                R.id.personal_area -> {
+                    loadFragment(AreaFragment())
+                    true
+                }
+            }
+        }
     }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
 }
